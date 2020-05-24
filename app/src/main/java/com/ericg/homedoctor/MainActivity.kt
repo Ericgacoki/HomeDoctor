bd86c78ec4c788d74b9d93cb6a3ca364e2f7a273
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-05-24 15:02:48 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ericg/homedoctor/MainActivity.kt
+++ app/src/main/java/com/ericg/homedoctor/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-// todo(): continue coding here
+// todo(): continue coding before july
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
