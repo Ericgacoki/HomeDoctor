7a05feb7a9433e5105947979af57f6b8b22cc4d1
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-08-05 01:09:54 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ericg/homedoctor/MainActivity.kt
+++ app/src/main/java/com/ericg/homedoctor/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ericg.homedoctor
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Bundles
 
 class MainActivity : AppCompatActivity() {
 // todo(): continue the project
